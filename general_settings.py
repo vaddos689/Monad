aa2ab245bd74cc55ce3a8a4b20518e62efce1e96
vaddos689 <roganfanboy@gmail.com>
2025-03-06 19:08:29 +0300

    Create README.md

--- general_settings.py
+++ general_settings.py
@@ -1,8 +1,8 @@
 import asyncio
 
-APRIORI_STAKE_RANGE = [0.0001, 0.0002]
+APRIORI_STAKE_RANGE = [0.0001, 0.0002] # MON selection range for staking
 
-AICRAFTREFCODE = ''
-AICRAFT_VOTES_COUNT = 1 # Сколько раз голосовать
+AICRAFTREFCODE = '' # your aicraft refcode for sign-in
+AICRAFT_VOTES_COUNT = 1 # number of votes per account
 
-semaphore = asyncio.Semaphore(1) # Кол-во одновременно работаюших аккаунтов
+semaphore = asyncio.Semaphore(1) # number of simultaneously working accounts
